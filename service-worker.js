69d48d13024f93661b00b84089feed13054aa257
HadsRadDad <40368339+HadsRadDad@users.noreply.github.com>
2019-07-26 14:12:17 -0500

    work

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,29 @@ const cacheName = 'ipad.v0.8';
 
 const cacheAssets = [
   'index.html',
-  './disciplines/Fire.html'
+  '/disciplines/Fire.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Education.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/EmergencyMan.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/EMS.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/GovAdmin.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Hazmat.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Healthcare.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Indirect.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/IT.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Law.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Other.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/PublicCom.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/PublicHealth.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/PublicWorks.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Safety.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Search.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Trans.html',
+  'https://hadsraddad.github.io/CDPDisplay/disciplines/Vol.html',
+  'ipadbg.png',
+  'coursestyle.css',
+  'Disciplines.otf',
+  'indexstyle.css',
+  'CDP.mp4'
 ];
 
 // Call install event
