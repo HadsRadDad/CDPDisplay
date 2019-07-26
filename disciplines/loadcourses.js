77ede3f04130bf386c061c5430e9e76476fc93e7
HadsRadDad <40368339+HadsRadDad@users.noreply.github.com>
2019-07-26 12:46:18 -0500

    upload

---
+++ disciplines/loadcourses.js
@@ -0,0 +1,244 @@
+	$("#ARIO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ARIO.txt");
+    });
+	
+	$("#ASTTP").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ASTTP.txt");
+    });
+	
+	$("#BPAC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/BPAC.txt");
+    });
+	
+	$("#BTM").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/BTM.txt");
+    });
+	
+	$("#BMAP").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/BMAP.txt");
+    });
+	
+    $("#CSM").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/CSM.txt");
+    });
+	
+	$("#EHTER").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/EHTER.txt");
+    });
+	
+	$("#EMO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/EMO.txt");
+    });
+
+	$("#EMRA").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/EMRA.txt");
+    });
+	
+	$("#ERDB").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ERDB.txt");
+	});
+	
+	$("#ERHM").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ERHM.txt");
+    });
+	
+	$("#FFC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/FFC.txt");
+    });
+	
+	$("#FFCE").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/FFCE.txt");
+    });
+	
+	$("#FFE").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/FFE.txt");
+    });
+	
+	$("#FFO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/FFO.txt");
+    });
+	
+	$("#FRAME").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/FRAME.txt");
+    });
+		
+	$("#HARM").click(function(){
+		$("#classinfo").load("http://localhost/courseinfo/HARM.txt");
+    });
+	
+	$("#HMO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HMO.txt");
+    });
+	
+	$("#HCL").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HCL.txt");
+    });
+	
+	$("#HCRL").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HCRL.txt");
+    });
+	
+	$("#HEC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HEC.txt");
+    });
+	
+	$("#HERO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HERO.txt");
+    });
+	
+	$("#HERT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HERT.txt");
+    });
+	
+	$("#HERTBTTT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HERTBTTT.txt");
+    });
+	
+	$("#HID").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HID.txt");
+    });
+	
+	$("#HMT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HMT.txt");
+    });
+	
+	$("#HMBR").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HMBR.txt");
+    });
+
+	$("#HOT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HOT.txt");
+    });
+	
+	$("#HOTI").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HOTI.txt");
+    });
+	
+	$("#HOTLE").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HOTLE.txt");
+    });
+	
+	$("#HT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HT.txt");
+    });
+	
+	$("#IC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/IC.txt");
+    });
+	
+	$("#ICR").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ICR.txt");
+    });
+	
+	$("#IED").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/IED.txt");
+    });
+	
+	$("#ILERSBA").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ILERSBA.txt");
+    });
+	
+	$("#ITC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/ITC.txt");
+    });
+	
+	$("#LEPM").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/LEPM.txt");
+    });
+	
+	$("#LEPMTTT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/LEPMTTT.txt");
+    });
+	
+	$("#LERA").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/LERA.txt");
+    });	
+	
+	$("#PODET").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/PODET.txt");
+    });	
+	
+	$("#MPI").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/MPI.txt");
+    });
+
+	$("#PMC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/PMC.txt");
+    });
+	
+	$("#PPMB").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/PPMB.txt");
+    });
+	
+	$("#PPMBTTT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/PPMBTTT.txt");
+    });
+	
+	$("#RAAC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RAAC.txt");
+    });
+	
+	$("#RADTTT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RADTTT.txt");
+    });
+	
+	$("#RCCC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RCCC.txt");
+    });
+	
+	$("#RDIR").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RDIR.txt");
+    });
+	
+	$("#RECC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RECC.txt");
+    });
+	
+	$("#REEC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/REEC.txt");
+    });
+	
+	$("#RERO").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RERO.txt");
+    });
+	
+	$("#RP").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RP.txt");
+    });
+
+	$("#RPCC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RPCC.txt");
+    });
+	
+	$("#RCTIC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RCTIC.txt");
+    });
+	
+	$("#RPPR").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/RPPR.txt");
+    });
+	
+	$("#SAAC").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/SAAC.txt");
+    });
+	
+	$("#SAAT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/SAAT.txt");
+    });
+
+	$("#SAT").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/SAT.txt");
+    });
+	
+	$("#SNS").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/SNS.txt");
+    });	
+	
+	$("#HMA").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/HMA.txt");
+    });	
+	
+	$("#VBIED").click(function(){
+        $("#classinfo").load("http://localhost/courseinfo/VBIED.txt");
+    });
+
---
+++ service-worker.js
@@ -0,0 +1,68 @@
+const cacheName = 'ipad.v1';
+
+const cacheAssets = [
+  'flexbox.html',
+  './disciplines/Fire.html',
+  './disciplines/Education.html',
+  './disciplines/EmergencyMan.html',
+  './disciplines/EMS.html',
+  './disciplines/GovAdmin.html',
+  './disciplines/Hazmat.html',
+  './disciplines/Healthcare.html',
+  './disciplines/Indirect.html',
+  './disciplines/IT.html',
+  './disciplines/Law.html',
+  './disciplines/Other.html',
+  './disciplines/PublicCom.html',
+  './disciplines/PublicHealth.html',
+  './disciplines/PublicWorks.html',
+  './disciplines/Safety.html',
+  './disciplines/Search.html',
+  './disciplines/Trans.html',
+  './disciplines/Vol.html',
+  'ipadbg.png',
+  'coursestyle.css',
+  'Disciplines.otf',
+  'indexstyle.css',
+  'CDP.mp4',
+  '/media/*'
+];
+
+// Call install event
+self.addEventListener('install', e =>{
+  console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call activate Event
+self.addEventListener('install', e =>{
+  console.log('Service Worker: Activated');
+  // Remove old caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if(cache !== cacheName) {
+            console.log('Service Working: Clearing cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
