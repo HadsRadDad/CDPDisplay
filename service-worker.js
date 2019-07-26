9ac20c9d166eb031c91793485be37631041cf891
HadsRadDad <40368339+HadsRadDad@users.noreply.github.com>
2019-07-26 14:08:02 -0500

    sw

--- service-worker.js
+++ service-worker.js
@@ -2,29 +2,7 @@ const cacheName = 'ipad.v0.8';
 
 const cacheAssets = [
   'index.html',
-  './disciplines/Fire.html',
-  './disciplines/Education.html',
-  './disciplines/EmergencyMan.html',
-  './disciplines/EMS.html',
-  './disciplines/GovAdmin.html',
-  './disciplines/Hazmat.html',
-  './disciplines/Healthcare.html',
-  './disciplines/Indirect.html',
-  './disciplines/IT.html',
-  './disciplines/Law.html',
-  './disciplines/Other.html',
-  './disciplines/PublicCom.html',
-  './disciplines/PublicHealth.html',
-  './disciplines/PublicWorks.html',
-  './disciplines/Safety.html',
-  './disciplines/Search.html',
-  './disciplines/Trans.html',
-  './disciplines/Vol.html',
-  'ipadbg.png',
-  'coursestyle.css',
-  'Disciplines.otf',
-  'indexstyle.css',
-  'CDP.mp4'
+  '/disciplines/Fire.html'
 ];
 
 // Call install event
